5bae8c1ed80b9898dde55587d4fcc6724f0b04fd
Gordon Fleming <flemingrgordon@gmail.com>
2021-07-09 09:50:29 +0200

    better

--- src/lib/Components/logo.svelte
+++ src/lib/Components/logo.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div id="logo-box">
-    <svg transition:fade="{{duration:2500}}" version="1.1" id="thinkTeacherLogo" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+    <svg in:fade="{{duration:2500}}" version="1.1" id="thinkTeacherLogo" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
         viewBox="0 0 853.1 198.8" style="enable-background:new 0 0 853.1 198.8;" xml:space="preserve">
     <style type="text/css">
         .st0{fill:#ED8D1B;}
