dfc5572ec7f9da130a50414ee3be9c0907da36c5
Gordon Fleming <flemingrgordon@gmail.com>
2021-12-08 13:17:16 +0200

    webinar bar

---
+++ src/lib/Components/WebinarsBar.svelte
@@ -0,0 +1,45 @@
+<script>
+    export let webinarData
+</script>
+
+<div class="container">
+    <div class="row grey-grad big-gap">
+        <h2 class="text-center text-blue">Webinars</h2>
+        {#each webinarData as webinar}
+            <div class="col-md-4 col-sm-12 mt-2">
+                <div class="frame-wrapper">
+                    <iframe src="{webinar.link}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+        {/each}
+    </div>
+</div>
+
+<style>
+    iframe{
+        position: absolute;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        width: 100%;
+        height: 100%;
+        padding: 5px;
+
+    }
+    .frame-wrapper{
+        position: relative;
+        background-color: var(--logo-grey);
+        width: 100%;
+        height: 100%;
+        border-radius: 5px;
+        border: 3px solid var(--logo-gold);
+
+        padding-top: 56.25%; /* 16:9 Aspect Ratio (divide 9 by 16 = 0.5625) */
+        overflow: hidden;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+    }
+</style>
--- src/routes/webinars.svelte
+++ src/routes/webinars.svelte
@@ -42,8 +42,10 @@
         {:else}
             {#each webinars as webinar}
                 <div class="col-lg-6 col-md-12 mt-2">
-                    <div class="frame-wrapper">
-                        <iframe src="{webinar.link}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div class="webinar-wrapper">
+                        <div class="frame-wrapper">
+                            <iframe src="{webinar.link}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
                         <p class="text-white mt-2">{webinar.description}</p>
                     </div>
                 </div>
@@ -55,6 +57,7 @@
 
 <style>
     iframe{
+        position: absolute;
         max-width: 800px;
         top: 0;
         left: 0;
@@ -65,11 +68,20 @@
         text-align: justify;
     }
     .frame-wrapper{
-        background-color: var(--logo-grey);
-        width: 100%;
-        height: 100%;
+        position: relative;
+        padding-top: 56.25%; /* 16:9 Aspect Ratio (divide 9 by 16 = 0.5625) */
+        overflow: hidden;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+    }
+    .webinar-wrapper{
         padding: 3%;
+        position: relative;
+        background-color: var(--logo-grey);
         border-radius: 5px;
         border: 3px solid var(--logo-gold);
+        height: 100%;
     }
 </style>
