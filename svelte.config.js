bc456d8276e0766294d7d86c8f8b10990ba6affb
Gordon Fleming <flemingrgordon@gmail.com>
2022-02-23 18:28:33 +0200

    plz-old

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,9 @@
+
 import adapter from '@sveltejs/adapter-netlify';
 
 export default {
 	kit: {
-		adapter: adapter({
-			// if true, will split your app into multiple functions
-			// instead of creating a single one for the entire app
-			split: false
-		})
+		adapter: adapter(), // currently the adapter does not take any options
+		target: '#svelte'
 	}
 };
