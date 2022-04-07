3d79fae85def1804864d79806fc5f189e974d3d8
Gordon Fleming <flemingrgordon@gmail.com>
2022-04-07 20:57:07 +0200

    Update payment.svelte

--- src/routes/payment.svelte
+++ src/routes/payment.svelte
@@ -1,82 +1,93 @@
 <script>
 	import { onMount } from "svelte";
 	import { id } from "$lib/stores";
+	import axios from "axios";
+	import { API_URL } from "$lib/env.js";
+	import { Jumper } from "svelte-loading-spinners";
+	import Icon from "svelte-awesome";
+	import { checkCircleO } from "svelte-awesome/icons";
 
-	if ($id === undefined) {
-		$id = localStorage.getItem("id");
-	}
+	// TODO : show any errors to user... add user name, surname and any others along with the meta you want like ttcode...
 
-	// Replace the supplied `publicKey` with your own.
-	// Ensure that in production you use a production public_key.
 	let sdk,
 		inline,
 		form,
+		loading = true,
+		paying = false,
 		submitButton = true,
 		amountInCents = 36000,
-		tokenCreated = false,
-		amountInRands = amountInCents / 100;
+		amountInRands = amountInCents / 100,
+		errMsg,
+		successMsg;
+
 	onMount(async () => {
+		if ($id === undefined) {
+			$id = localStorage.getItem("id");
+		}
+
 		sdk = new window.YocoSDK({
 			publicKey: "pk_test_7322f2efgOezbbk123c4",
 		});
 
-		// Create a new dropin form instance
 		inline = sdk.inline({
 			layout: "basic",
 			amountInCents: amountInCents,
 			currency: "ZAR",
+			showErrors: true,
 		});
 
-		// this ID matches the id of the element we created earlier.
 		inline.mount("#card-frame");
-		// Run our code when your form is submitted
 		form = document.getElementById("payment-form");
+		loading = false;
 	});
 
 	let token;
-	async function submit() {
-		// Disable the button to prevent multiple clicks while processing
+	async function makePayment() {
 		submitButton = false;
-		// This is the inline object we created earlier with the sdk
 		inline
 			.createToken()
 			.then(function (result) {
-				// Re-enable button now that request is complete
-				// (i.e. on success, on error and when auth is cancelled)
 				submitButton = true;
 				if (result.error) {
 					const errorMessage = result.error.message;
-					errorMessage && alert("error occured: " + errorMessage);
+					console.log(errorMessage);
 				} else {
 					token = result;
-					alert("card successfully tokenised: " + token.id);
-					tokenCreated = true;
+					paying = true;
+					console.log("card successfully tokenised: " + token.id);
 				}
 			})
 			.catch(function (error) {
-				// Re-enable button now that request is complete
 				submitButton = true;
 				alert("error occured: " + error);
 			});
 
-		if (tokenCreated) {
-			await axios
+		inline.on("card_tokenized", function (token) {
+			console.log("restsdfgsdgs", token);
+			errMsg = undefined;
+			successMsg = undefined;
+			axios
 				.post(`${API_URL}/payments`, {
 					amount_in_cents: amountInCents,
-					token: token,
+					token: token.id,
+					payed: true,
 					users_permissions_user: {
 						id: $id,
 					},
 				})
 				.then((response) => {
+					successMsg = "Success, payment has been made!";
 					console.log(response);
+					paying = false;
 				})
 				.catch((error) => {
+					errMsg = error.response.data.message.error.displayMessage;
 					console.log("An error occurred:", error.response.data);
+					paying = false;
 				});
-		}
+			form.reset();
+		});
 	}
-	// Any additional form data you want to submit to your backend should be done here, or in another event listener
 </script>
 
 <svelte:head>
@@ -86,26 +97,68 @@
 
 <div class="container mt-5">
 	<div class="row">
+		<div class="text-center">
+			<h2 class="mb-4">Membership <span class="text-blue">Payment</span></h2>
+			{#if successMsg !== undefined}
+				<h4 class="success-col">{successMsg}</h4>
+				<Icon class="success-col" data={checkCircleO} scale="2.4" />
+			{/if}
+			{#if errMsg !== undefined}
+				<h4 class="error-col">{errMsg}</h4>
+			{/if}
+
+			{#if loading || paying}
+				<div class="d-flex justify-content-center mt-5">
+					<Jumper size="150" color="#5C677D" unit="px" duration="1s" />
+				</div>
+
+				{#if paying}
+					<h3 class="text-center">We are busy processing your payment...</h3>
+				{/if}
+			{/if}
+		</div>
+
 		<div class="col d-flex justify-content-center">
 			<form id="payment-form">
 				<div class="one-liner">
 					<div id="card-frame">
 						<!-- Yoco Inline form will be added here -->
 					</div>
-					<button
-						id="pay-button"
-						class:bg-gold={submitButton}
-						class:cta={submitButton}
-						class="btn btn-sm shadow mt-3"
-						on:click|preventDefault={submit}
-						disabled={!submitButton}
-					>
-						PAY - R {amountInRands}
-					</button>
+					<div class="text-center mt-4 mb-4">
+						<button
+							id="pay-button"
+							class:bg-gold={submitButton}
+							class:cta={submitButton}
+							class="btn btn-lg shadow"
+							on:click|preventDefault={makePayment}
+							disabled={!submitButton}
+						>
+							PAY - R {amountInRands}
+						</button>
+					</div>
 				</div>
 				<p class="success-payment-message" />
 			</form>
 		</div>
+	</div>
+	<hr class="rounded" />
+	<div class="text-center mt-4 mb-3">
+		<img
+			class="img-fluid mb-3"
+			id="yoco"
+			src="https://strapi-upload-s3.glass.thinkteacher.co.za/media/yoco_colour_d4694a7164.svg"
+			alt="yoco"
+		/><br />
+		<img
+			class="img-fluid payment-logo"
+			src="https://strapi-upload-s3.glass.thinkteacher.co.za/media/visa_colour_0104f4de82.svg"
+			alt="visa"
+		/>
+		<img
+			class="img-fluid payment-logo"
+			src="https://strapi-upload-s3.glass.thinkteacher.co.za/media/mastercard_colour_446709c296.svg"
+			alt="mastercard"
+		/>
 	</div>
 </div>
 
@@ -118,5 +171,19 @@
 	}
 	.bg-gold {
 		background-color: var(--logo-gold);
+	}
+	.payment-logo {
+		max-width: 55px;
+		margin-right: 20px;
+		margin-left: 20px;
+	}
+	#yoco {
+		max-width: 120px;
+	}
+	hr.rounded {
+		border-top: 4px solid #bbb;
+		border-radius: 3px;
+		max-width: 20vw;
+		margin: 0 auto;
 	}
 </style>
