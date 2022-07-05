938613b23259e1e1aa17ca25aec619f4e6f508d6
Gordon Fleming <flemingrgordon@gmail.com>
2022-07-05 21:46:12 +0200

    netlify fix

--- svelte.config.js
+++ svelte.config.js
@@ -2,9 +2,18 @@ import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
-	}
+    kit: {
+        adapter: adapter({
+            // if true, will create a Netlify Edge Function rather
+            // than using standard Node-based functions
+            edge: false,
+
+            // if true, will split your app into multiple functions
+            // instead of creating a single one for the entire app.
+            // if `edge` is true, this option cannot be used
+            split: false
+        }),
+    }
 };
 
 export default config;
