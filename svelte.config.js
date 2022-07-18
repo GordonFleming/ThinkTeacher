7ae7f725c219f1ce79e2fd8ddf9f74b418bef655
Gordon Fleming <flemingrgordon@gmail.com>
2022-07-18 23:06:28 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,12 +3,11 @@ import adapter from '@sveltejs/adapter-auto';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-	},
-    prerender: {
+		adapter: adapter({prerender: {
         default: true,
         entries: ['*', '/robots.txt', '/sitemap.xml']
-      }
+      }}),
+	},
 };
 
 export default config;
