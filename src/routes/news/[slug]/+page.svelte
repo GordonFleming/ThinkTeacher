ffeaba4e508807ac44411cad3a8f63725f832020
Gordon Fleming <flemingrgordon@gmail.com>
2022-09-06 10:51:28 +0200

    remove svelte-pdf in favour of iframe

--- src/routes/news/[slug]/+page.svelte
+++ src/routes/news/[slug]/+page.svelte
@@ -1,81 +1,76 @@
 <script>
-	import Icon from "$lib/Icons/icon.svelte";
-	import { arrowLeft } from "$lib/Icons/icons";
-	import SvelteMarkdown from "svelte-markdown";
-	import { onMount } from "svelte";
-	import { Jumper } from "svelte-loading-spinners";
+    import Icon from "$lib/Icons/icon.svelte";
+    import { arrowLeft } from "$lib/Icons/icons";
+    import SvelteMarkdown from "svelte-markdown";
 
-	let PdfViewer;
+    export let data;
+    let { post } = data;
+    let date;
+    let publish;
+    let source;
 
-	onMount(async () => {
-		const module = await import("svelte-pdf");
-		PdfViewer = module.default;
-		loading = false;
-	});
-
-	export let data;
-	let { post } = data;
-	let date;
-	let publish;
-	let source;
-	let loading = true;
-
-	date = new Date(post.published_at);
-	publish = date.toLocaleString("en-ZA", { month: "long", day: "2-digit", year: "numeric" });
-	source = post.content;
+    date = new Date(post.published_at);
+    publish = date.toLocaleString("en-ZA", { month: "long", day: "2-digit", year: "numeric" });
+    source = post.content;
 </script>
 
 <svelte:window />
 
 <svelte:head>
-	<title>{post.title}</title>
+    <title>{post.title}</title>
 </svelte:head>
 
 <div class="container">
-	<a sveltekit:prefetch href="/news"><Icon data={arrowLeft} scale="3" fill="#4F5D89" /></a>
-	<h1 class="text-center mb-4" style="margin-top: 0;">{post.title}</h1>
-	<div class="text-center">
-		{#if loading}
-			<div class="d-flex justify-content-center mt-5">
-				<Jumper size="150" color="#5C677D" unit="px" duration="1s" />
-			</div>
-		{:else}
-			<svelte:component
-				this={PdfViewer}
-				scale="1.4"
-				showBorder="false"
-				showButtons="false"
-				url={post.pdf.url}
-			/>
-		{/if}
-	</div>
+    <a sveltekit:prefetch href="/news"><Icon data={arrowLeft} scale="3" fill="#4F5D89" /></a>
+    <h1 class="text-center mb-4" style="margin-top: 0;">{post.title}</h1>
+    <div class="text-center">
+        <div class="responsive-iframe-container">
+            <iframe src={post.pdf.url} title={post.title} class="responsive-iframe" />
+        </div>
+    </div>
 
-	<time datetime={publish}>{publish}</time>
+    <time datetime={publish}>{publish}</time>
 
-	<div id="mark-down">
-		<SvelteMarkdown {source} />
-	</div>
+    <div id="mark-down">
+        <SvelteMarkdown {source} />
+    </div>
 </div>
 
 <style>
-	h1 {
-		color: var(--logo-gold);
-	}
-	time {
-		color: #fff;
-	}
-	.container {
-		border-radius: 20px;
-	}
-	@media screen and (max-width: 1000px) {
-		.container {
-			padding: 1rem;
-		}
-	}
+    .responsive-iframe-container {
+        position: relative;
+        overflow: hidden;
+        width: 100%;
+        padding-top: 100%;
+    }
+    .responsive-iframe {
+        position: absolute;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        width: 100%;
+        height: 100%;
+    }
 
-	@media screen and (min-width: 999px) {
-		.container {
-			padding: 5rem;
-		}
-	}
+    h1 {
+        color: var(--logo-gold);
+    }
+    time {
+        color: #fff;
+    }
+    .container {
+        border-radius: 20px;
+    }
+    @media screen and (max-width: 1000px) {
+        .container {
+            padding: 1rem;
+        }
+    }
+
+    @media screen and (min-width: 999px) {
+        .container {
+            padding: 5rem;
+        }
+    }
 </style>
--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -7,7 +7,6 @@ export async function GET() {
   const partners = res.data
   const pages = [`about`, `benefits`, `partners`, `news`, `webinars`, `contact-us`, `login`, `register`]
   const body = sitemap(partners, pages)
-  console.log(body);
 
   const headers = {
     'Cache-Control': 'max-age=0, s-maxage=3600',
