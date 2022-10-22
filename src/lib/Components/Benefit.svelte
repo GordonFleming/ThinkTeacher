b57d810f9bb0abd328e72029dcff7c637f1638fb
Gordon Fleming <flemingrgordon@gmail.com>
2022-10-22 14:12:58 +0200

    packages (benefits) migrated

--- src/lib/Components/Benefit.svelte
+++ src/lib/Components/Benefit.svelte
@@ -1,78 +1,81 @@
 <script>
-	import { goto } from "$app/navigation";
-	import { travelType, courseType } from "$lib/stores";
+    import { goto } from "$app/navigation";
+    import { travelType, courseType } from "$lib/stores";
 
-	export let benefitData;
+    export let benefitData;
 
-	function redirectPlusDataCompute(type) {
-		switch (benefitData[0].partner.company_name.toLowerCase()) {
-			case "cirrus travel":
-				$travelType = type.toLowerCase().replace(" ", "_");
-				goto("/auth/form/travel");
-				break;
-			case "macmillan":
-				$courseType = type;
-				goto("/auth/form/courses");
-				break;
-			case "kim forbes":
-				goto("/auth/form/wellbeing");
-				break;
-			case "sa health brokers (pty) ltd":
-				goto("/auth/form/medical-aid");
-				break;
-			case "alexander forbes":
-				goto("/auth/form/finance");
-				break;
-			case "stephen g. may":
-				goto("/auth/form/legal");
-				break;
-			case "kia":
-				goto("/auth/form/cars");
-				break;
-			default:
-				goto("/");
-		}
-	}
+    function redirectPlusDataCompute(type) {
+        switch (benefitData[0].attributes.partner.data.attributes.company_name.toLowerCase()) {
+            case "cirrus travel":
+                $travelType = type.toLowerCase().replace(" ", "_");
+                goto("/auth/form/travel");
+                break;
+            case "macmillan":
+                $courseType = type;
+                goto("/auth/form/courses");
+                break;
+            case "kim forbes":
+                goto("/auth/form/wellbeing");
+                break;
+            case "sa health brokers (pty) ltd":
+                goto("/auth/form/medical-aid");
+                break;
+            case "alexander forbes":
+                goto("/auth/form/finance");
+                break;
+            case "stephen g. may":
+                goto("/auth/form/legal");
+                break;
+            case "kia":
+                goto("/auth/form/cars");
+                break;
+            default:
+                goto("/");
+        }
+    }
 </script>
 
 {#if benefitData.length > 0}
-	<div class="row mt-2 justify-content-center">
-		{#each benefitData as benData}
-			<div class="col-sm-12 col-md-10 col-lg-6">
-				<div class="card bg-dark m-2 shadow-lg">
-					<img
-						class="img-fluid rounded cta"
-						src="https://strapi-upload-s3.glass.thinkteacher.co.za/media/{benData.banner
-							.hash}{benData.banner.ext}"
-						alt="cover"
-						on:click={() => redirectPlusDataCompute(benData.name)}
-					/>
-					<div class="card-body">
-						<h3 class="card-title text-logo-gold">
-							Think <span class="text-lighter-blue">{benData.name}</span>
-						</h3>
-						<p class="card-text">
-							{@html benData.description}
-						</p>
-						<button
-							class="btn bg-gold shadow cta text-black fs-5 p-1"
-							on:click={() => redirectPlusDataCompute(benData.name)}>Enquire</button
-						>
-					</div>
-					<div class="card-footer">
-						<span class="badge bg-light">{benData.partner.company_name}</span>
-					</div>
-				</div>
-			</div>
-		{/each}
-	</div>
+    <div class="row mt-2 justify-content-center">
+        {#each benefitData as benData}
+            <div class="col-sm-12 col-md-10 col-lg-6">
+                <div class="card bg-dark m-2 shadow-lg">
+                    <img
+                        class="img-fluid rounded cta"
+                        src={benData.attributes.banner.data.attributes.url}
+                        alt="cover"
+                        on:click={() => redirectPlusDataCompute(benData.attributes.name)}
+                        on:keydown={() => redirectPlusDataCompute(benData.attributes.name)}
+                    />
+                    <div class="card-body">
+                        <h3 class="card-title text-logo-gold">
+                            Think <span class="text-lighter-blue">{benData.attributes.name}</span>
+                        </h3>
+                        <p class="card-text">
+                            {@html benData.attributes.description}
+                        </p>
+                        <button
+                            class="btn bg-gold shadow cta text-black fs-5 p-1"
+                            on:click={() => redirectPlusDataCompute(benData.attributes.name)}
+                            >Enquire</button
+                        >
+                    </div>
+                    <div class="card-footer">
+                        <span class="badge bg-light"
+                            >{benData.attributes.partner.data.attributes.company_name}</span
+                        >
+                    </div>
+                </div>
+            </div>
+        {/each}
+    </div>
 {:else}
-	<h4>No packages yet.</h4>
+    <h4>No packages yet.</h4>
 {/if}
 
 <style>
-	.card {
-		height: 92%;
-		padding: 3%;
-	}
+    .card {
+        height: 92%;
+        padding: 3%;
+    }
 </style>
--- src/routes/benefits/+page.server.js
+++ src/routes/benefits/+page.server.js
@@ -13,49 +13,64 @@ let health_cat = 3,
 	glasses_cat = 8,
 	business_cat = 9;
 if (prod === "true") {
-	(health_cat = 2),
-		(travel_cat = 1),
-		(course_cat = 6),
+	(health_cat = 5),
+		(travel_cat = 2),
+		(course_cat = 7),
 		(wellbeing_cat = 3),
-		(finance_cat = 5),
+		(finance_cat = 6),
 		(legal_cat = 4),
-		(books_cat = 7),
-		(cars_cat = 8),
-		(glasses_cat = 9),
-		(business_cat = 10);
+		(books_cat = 8),
+		(cars_cat = 9),
+		(business_cat = 11);
 }
 
 export async function load() {
 	const endpoint = `graphql`;
 	const graphqlQuery = {
-		operationName: "fetchBenefits",
-		query: `query fetchBenefits {     
-                packages {
-                    name,
-                    description,
-                    partner {
-                        company_name,
+		operationName: "fetchPackages",
+		query: `query fetchPackages {  
+            packages (pagination: { start: 0, limit: -1}) {
+                data {
+                    attributes {
+                        name,
                         description,
-                        category{
-                            id,
+                        details,
+                        banner {
+                                data {
+                                    attributes {
+                                        url
+                                }
+                            }
                         },
-                        logo{
-                            url
-                        },
-                        slug
-                    },
-                    details,
-                    banner{
-                        hash,
-                        ext
+                        partner {
+                            data {
+                                attributes {
+                                    company_name,
+                                    description,
+                                    category {
+                                        data {
+                                            id
+                                        }
+                                    },
+                                    logo {
+                                        data {
+                                            attributes {
+                                                    url
+                                            }
+                                        }
+                                    }
+                                    slug
+                                }
+                            }
+                        }
                     }
                 } 
-            }`,
+            }		
+        }`,
 		variables: {},
 	};
 
 	const res = await apiGraph('POST', endpoint, graphqlQuery);
-
 	let packages = [],
 		travel = [],
 		health = [],
@@ -71,10 +86,11 @@ export async function load() {
 
 	if (res.ok) {
 		const data = await res.json();
-		packages = data.data.packages;
+		packages = data.data.packages.data;
+        // console.log(packages)
 
 		function seperatePackages(item) {
-			switch (parseInt(item.partner.category.id)) {
+			switch (parseInt(item.attributes.partner.data.attributes.category.data.id)) {
 				case travel_cat:
 					travel.push(item);
 					break;
